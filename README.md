f2c2e20f7722d3efad93b2b81d3925025cbb350f
Mathijs Moerman <mathijs.moerman@qrama.io>
2016-12-23 16:25:17 +0100

    Updated structure

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Info
-This is a subordinate charm for the Sojobo-api which enables the use of MAAS-clouds
+This is a subordinate charm for the Sojobo-api which enables the testbench
 
 # Usage
-To enable MAAS-clouds:
+To enable the testbench:
 * Deploy the charm
 * Add a relation to the Sojobo-api
-To disable MAAS-clouds, just remove the application.
-**Warning: Removing this will prevent the use of existing MAAS-clouds!**
 
 # Bugs
 Report bugs on <a href="https://github.com/Qrama/controller-MAAS/issues">Github</a>
--- reactive/controller-maas.py
+++ reactive/controller-maas.py
@@ -27,7 +27,7 @@ from charmhelpers.core.host import service_restart
 @when_not('controller-maas.installed')
 def install():
     api_dir = requests.get('http://localhost:5000').json()['message']['api_dir']
-    copyfile('{}/files/controller_maas.py'.format(charm_dir()), '{}/api/controller_maas.py'.format(api_dir))
+    copyfile('{}/files/controller_maas.py'.format(charm_dir()), '{}/controllers/controller_maas.py'.format(api_dir))
     service_restart('sojobo-api')
     status_set('active', 'data copied')
     set_state('controller-maas.installed')
